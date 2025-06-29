2c81055ae6e6b0f9919848dcae5560a105b7607a
Istallia <55042065+istallia@users.noreply.github.com>
2025-06-29 16:54:02 +0900

    [Improve] ガレージのURLが変更されたので追従

--- content-script.js
+++ content-script.js
@@ -33,11 +33,18 @@ const putCopyButton = () => {
 	/* 追加済み判定 */
 	if (document.getElementById('ista-button-copy_liked_users')) return;
 	/* 「いいね！したユーザー」のボタンの存在確認 */
-	const svg_liked = document.querySelector('button.MuiButtonBase-root > span > svg[class]');
-	if (!svg_liked) return;
+	const svg_liked_candidates = Array.from(document.querySelectorAll('button.MuiButtonBase-root > span > svg[class]'));
+	const filtered             = svg_liked_candidates.filter(svg => {
+		const button = svg.parentNode.parentNode;
+		return button.querySelector('span[class*="Ripple"]');
+	});
+	console.log(filtered);
+	if (filtered.length === 0) return;
+	const svg_liked      = filtered[0];
 	const button_liked   = svg_liked.parentNode.parentNode;
 	const svg_span_liked = svg_liked.parentNode;
-	const ripple_liked   = button_liked.querySelector('span[class*="Ripple"]').cloneNode(true);
+	const ripple_elem    = button_liked.querySelector('span[class*="Ripple"]');
+	const ripple_liked   = ripple_elem.cloneNode(true);
 	const listDiv        = button_liked.parentNode;
 	/* 要素を準備 */
 	const button  = document.createElement('button');
